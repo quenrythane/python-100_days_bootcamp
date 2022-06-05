926a293347af1af4579637d90d2f1ae2405a388a
quenrythane <artur.babinski.g@gmail.com>
2022-06-05 12:01:38 +0200

    starting new day 15 projext

--- days_11-20/day15_coffee_machine/day15_coffee_machine.py
+++ days_11-20/day15_coffee_machine/day15_coffee_machine.py
@@ -1,1 +1,55 @@
-# TODO: testing to do feature
+from data import *
+from machine_functions import *
+
+money = 10
+print(money)
+
+# init
+want_order = "y"
+
+# program
+while want_order != "n":
+    want_order = input("Welcome to the coffee machine! Do you want to order coffee? (y/n): ")
+    if want_order == "n":
+        break
+
+    order = input(f"What do you want to order? ({'/'.join(x for x in MENU)}): ").lower()
+    print()
+
+    if order == "report":
+        report()
+
+    elif order == "add":
+        add_resources()
+
+    elif order in MENU:
+        # check if enough resources
+        if resources["water"] >= MENU[order]["ingredients"]["water"] \
+                and resources["milk"] >= MENU[order]["ingredients"]["milk"]\
+                and resources["coffee"] >= MENU[order]["ingredients"]["coffee"]:
+
+            # take payment
+            print(f"{order} cost is ${MENU[order]['cost']}.", end=" ")
+            amount = take_coins()
+
+            # make coffee
+            if amount >= MENU[order]["cost"]:
+                resources["water"] -= MENU[order]["ingredients"]["water"]
+                resources["milk"] -= MENU[order]["ingredients"]["milk"]
+                resources["coffee"] -= MENU[order]["ingredients"]["coffee"]
+                money += MENU[order]["cost"]
+
+                print(f"Here is your {order}")
+                print(f"you paid ${amount} and cost was ${MENU[order]['cost']}.", end=" ")
+                if amount > MENU[order]["cost"]:
+                    print(f"Here is your change ${float('{0:.2f}'.format(amount - MENU[order]['cost']))}")
+                print()
+
+            else:
+                print(f"Not enough money. Here is your money back ${amount}")
+
+        # if not enough
+        else:
+            print(f"Sorry, not enough resources to make {order} :(")
+    else:
+        print(f"Sorry, we don't have {order} :(")
---
+++ days_11-20/day15_coffee_machine/machine_functions.py
@@ -0,0 +1,30 @@
+from data import *
+
+def report():
+    for k, v in resources.items():
+        if k == "coffee":
+            print(f"{k}: {v}g")
+        else:
+            print(f"{k}: {v}ml")
+    print(f"Money: ${money}\n")
+
+
+def take_coins():
+    print("Please insert coins")
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickels = int(input("How many nickels?: "))
+    pennies = int(input("How many pennies?: "))
+    print()
+    return quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
+
+
+def add_resources():
+    print("Please add resources")
+    water = int(input("How much water?(ml): "))
+    milk = int(input("How much milk?(ml): "))
+    coffee = int(input("How much coffee?(g): "))
+    print()
+    resources["water"] += water
+    resources["milk"] += milk
+    resources["coffee"] += coffee
