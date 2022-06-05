f772a1dc4bff638d5332b9274c727b7e5d4db30a
quenrythane <artur.babinski.g@gmail.com>
2022-06-05 10:37:50 +0200

    starting new day 15 projext

---
+++ days_11-20/day15_coffee_machine/program_requirements.py
@@ -0,0 +1,24 @@
+# make 3 hot flavours
+# espresso: 50ml water, 18g coffee - 1.5$
+# latte: 200ml water, 24g coffee, 150ml  milk - 2.5$
+# cappuccino: 250ml water, 24g coffee, 100ml milk - 3.0$
+
+# machine resources
+# water: 300ml
+# milk: 200ml
+# coffee: 100g
+# money
+
+
+# coin operated
+# penny = 1 cents
+# nickel = 5 cents
+# dime = 10 cents
+# quarter = 25 cents
+
+
+# program requirements
+# 1 print report - how much resources are left
+# 2 check resources sufficient
+# 3 process coins - count value of coins and the change
+# 4 process order - check if resources sufficient, if yes, process order
--- days_11-20/fun_with_imports/main.py
+++ days_11-20/fun_with_imports/main.py
@@ -5,6 +5,7 @@ from func_var import *
 exercises around my more complex blackjack game
 """
 
+
 if __name__ == "__main__":
     var = func(var)
     print(var)
