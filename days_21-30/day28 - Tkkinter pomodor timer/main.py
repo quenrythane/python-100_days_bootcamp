6e2ab2ff74002aaaeea7ead8012cac7c47b855f7
quenrythane <artur.babinski.g@gmail.com>
2022-07-04 23:01:55 +0200

    added first shape of app

--- days_21-30/day28 - Tkkinter pomodor timer/main.py
+++ days_21-30/day28 - Tkkinter pomodor timer/main.py
@@ -1,5 +1,6 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -16,3 +17,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro timer")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+# highlightthickness hiding border between canvas and window
+pomodoro_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=pomodoro_img)
+canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+
+window.mainloop()
