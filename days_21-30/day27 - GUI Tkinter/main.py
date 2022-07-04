569fb7b2d1b86a21945ab2b34cb5fed1d8bae340
quenrythane <artur.babinski.g@gmail.com>
2022-07-04 21:20:23 +0200

    continue learning about tkinter -> buttons

---
+++ days_21-30/day27 - GUI Tkinter/main.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+FONT = "Arial", 24, "bold"
+
+window = tk.Tk()
+window.title("My first GUI Program")
+window.minsize(500, 300)
+
+# Labels
+my_label = tk.Label(text="I am a label", font=FONT)
+# changing cinfig of object
+my_label["text"] = "I am a label with new text"
+my_label.config(text="I am a label 2")
+# Packer
+my_label.pack(side="left")
+# my_label.pack(expand=True)
+
+# Button
+def button_clicked():
+    print("I got clicked")
+    text = "Button Got Clicked"
+    my_label["text"] = text
+
+button = tk.Button(text="Click Me", command=button_clicked)
+button.pack(side="left")
+
+# Entry
+
+
+
+window.mainloop()
