dc514399f91a4343598f1e6c9edc7c6b2fa90a8c
quenrythane <artur.babinski.g@gmail.com>
2022-07-04 21:56:17 +0200

    practice layout management

--- days_21-30/day27 - GUI Tkinter/Tkinter Layout Manager.py
+++ days_21-30/day27 - GUI Tkinter/Tkinter Layout Manager.py
@@ -4,15 +4,23 @@ FONT = "Arial", 24, "bold"
 
 window = tk.Tk()
 window.title("My first GUI Program")
-window.minsize(500, 300)
+window.minsize(600, 300)
+window.config(padx=100, pady=200)
+
+"""Layout Manager"""
+# Pack
+# Place
+# Grid - relative to others. When we use grid we could not use place
 
 # Labels
 my_label = tk.Label(text="I am a label", font=FONT)
-# changing cinfig of object
+# changing config of object
 my_label["text"] = "I am a label with new text"
 my_label.config(text="I am a label 2")
+my_label.config(padx=50, pady=50)
 # Packer
-my_label.pack(side="left")
+my_label.grid(column=1, row=2)
+# my_label.pack(side="left")
 # my_label.pack(expand=True)
 
 # Button
@@ -22,8 +30,12 @@ def button_clicked():
     my_label["text"] = text
 
 button = tk.Button(text="Click Me", command=button_clicked)
-button.pack(side="left")
+button.place(x=400, y=0)
 
 # Entry
 input = tk.Entry(width=10)
-input.pack()
+input.grid(column=0, row=0)
+
+
+window.mainloop()
+
