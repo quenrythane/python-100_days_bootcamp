e5c90eabf0b2df8303d5a0fdd3c555aa290ee799
quenrythane <artur.babinski.g@gmail.com>
2022-07-05 22:24:42 +0200

    add timer mechanism and work break states

--- days_21-30/day28 - Tkkinter pomodor timer/pomodoro_timer.py
+++ days_21-30/day28 - Tkkinter pomodor timer/pomodoro_timer.py
@@ -10,15 +10,37 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
+
+    if reps % 2 == 1:
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(WORK_MIN * 60)
+
+    elif reps % 8 == 0:
+        timer_label.config(text= "Break", fg=RED)
+        count_down(LONG_BREAK_MIN * 60)
+
+    elif reps % 2 == 0:
+        timer_label.config(text= "Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN * 60)
+
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
-    window.after(1000, count_down, count-1)
+    clock_text = "{:02}:{:02}".format(count//60, count%60)
+    canvas.itemconfig(timer_text, text=clock_text)
+    if count > 0:
+        window.after(1000, count_down, count-1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -34,7 +56,6 @@ canvas.create_image(100, 112, image=pomodoro_img)
 timer_text = canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-count_down(5)
 
 # timer label
 timer_label = Label(text="Timer", font=(FONT_NAME, 35, "bold"), bg=YELLOW, fg=GREEN, highlightthickness=0)
@@ -44,7 +65,7 @@ timer_label.grid(column=1, row=0)
 def start_clicked():
     pass
 
-start_button = Button(text="Start", command=start_clicked, highlightthickness=0)
+start_button = Button(text="Start", command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
 
 # reset button
