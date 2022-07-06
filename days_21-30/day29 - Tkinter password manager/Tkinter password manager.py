b0a33746dfa687589a55cb88c1e1cbca7da25243
quenrythane <artur.babinski.g@gmail.com>
2022-07-06 18:47:37 +0200

    add saving to csv option

--- days_21-30/day29 - Tkinter password manager/Tkinter password manager.py
+++ days_21-30/day29 - Tkinter password manager/Tkinter password manager.py
@@ -1,7 +1,17 @@
 from tkinter import *
+
+base_username = "artur.babinski.g@gmail.com"
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    with open("data_password.csv", "a") as data:
+        log = f"{website_input.get()},{username_input.get()},{password_input.get()}\n"
+        data.write(log)
+        website_input.delete(0, END), password_input.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -21,13 +31,17 @@ website_label.grid(column=0, row=1)
 
 website_input = Entry(width=40)
 website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
+
 
 # username section
 username_label = Label(text="Email/Username:")
 username_label.grid(column=0, row=2)
 
 username_input = Entry(width=40)
+username_input.insert(0, base_username)
 username_input.grid(column=1, row=2, columnspan=2)
+
 
 # password section
 password_label = Label(text="Password:")
@@ -40,8 +54,9 @@ password_input.grid(column=1, row=3)
 generate_button = Button(text="Generate Password", width=15)
 generate_button.grid(column=2, row=3)
 
+
 # add section
-add_button = Button(text="Add", width=34)
+add_button = Button(text="Add", width=34, command=save_password)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
