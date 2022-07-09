7e88ecb2da0619c8825246661d1b204499096e52
quenrythane <artur.babinski.g@gmail.com>
2022-07-09 21:10:04 +0200

    some work on day 32

--- days_31-40/day 32 - Automated Birthday Email/automated_birthday_email.py
+++ days_31-40/day 32 - Automated Birthday Email/automated_birthday_email.py
@@ -1,11 +1,21 @@
-from smtplib import *
-
-email_address = "arturappbrewery@gmail.com"
-email_password = "x6?5+((8kQB}qv-2"
-
-connection = SMTP('smtp.gmail.com', 587)
-connection.starttls()  # secure connection
+import smtplib
+import ssl
 
 
 
 
+receiver_email = "artur.babinski.g@gmail.com"
+email_subject = "Subject test"
+email_body = "Hello world test"
+email_message = f"Subject:{email_subject}\n\n{email_body}"
+
+
+"""
+1. turn on 2fa
+2. create an app password
+"""
+
+ssl_context = ssl.create_default_context()  # create secure socket layer context
+with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=ssl_context) as smtp_connection:  # start smpt
+    smtp_connection.login(user=SENDER, password=PASSWORD)  # login
+    smtp_connection.sendmail(from_addr=SENDER, to_addrs=receiver_email, msg=email_message)  # send message
