3cc2b173696e514df3525b2c13e4330765ff7af0
quenrythane <artur.babinski.g@gmail.com>
2022-07-09 16:08:30 +0200

    starting day 32

---
+++ days_31-40/day 32 - Automated Birthday Email/automated_birthday_email.py
@@ -0,0 +1,11 @@
+from smtplib import *
+
+email_address = "arturappbrewery@gmail.com"
+email_password = "x6?5+((8kQB}qv-2"
+
+connection = SMTP('smtp.gmail.com', 587)
+connection.starttls()  # secure connection
+
+
+
+
