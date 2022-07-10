7371d711f9580f6151601576fbaaf9a2b334c85a
quenrythane <artur.babinski.g@gmail.com>
2022-07-10 13:32:54 +0200

    finisged day 32

--- days_31-40/day 32 - Automated Birthday Email/birthdat_wihser_sender/main.py
+++ days_31-40/day 32 - Automated Birthday Email/birthdat_wihser_sender/main.py
@@ -1,11 +1,3 @@
-#To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
 from datetime import datetime
 import pandas
 import random
