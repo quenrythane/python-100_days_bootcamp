1cc93065619a737a1652db39e676cbd8fab17975
quenrythane <artur.babinski.g@gmail.com>
2022-07-21 21:47:24 +0200

    finished step 1

--- days_31-40/day 36 - stock follower/main_eh.py
+++ days_31-40/day 36 - stock follower/main_eh.py
@@ -1,8 +1,28 @@
-STOCK = "TSLA"
+import requests as req
+import json
+
+STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+STOCK_API_KEY = "TRA7J4P5E8UCUOR"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+stock_parameters ={
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+data = req.get(STOCK_ENDPOINT, params=stock_parameters).json()
+
+first_key, second_key = list(data["Time Series (Daily)"].keys())[:2]
+yesterday_price = float(data["Time Series (Daily)"][first_key]["4. close"])
+day_before_yesterday_price = float(data["Time Series (Daily)"][second_key]["4. close"])
+
+if abs(yesterday_price - day_before_yesterday_price) / yesterday_price >= 0.05:
+    print("Get News")
+
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
