d4aed89cdf04eb368513daef44409e03dc606915
quenrythane <artur.babinski.g@gmail.com>
2022-07-24 20:02:24 +0200

    finished connection

--- days_31-40/day 38 - workout tracker/workout_tracker.py
+++ days_31-40/day 38 - workout tracker/workout_tracker.py
@@ -2,27 +2,51 @@ import requests as req
 from datetime import datetime
 
 # get access data
-with open("access_data.txt", "r") as file:
-    APP_ID, API_KEY = file.read().split()
+GENDER = "male"
+WEIGHT_KG = 70
+HEIGHT_CM = 180
+AGE = 25
+with open("access_data_3.txt", "r") as file:
+    APP_ID, API_KEY, username, project_name, sheet_name = file.read().split()
 
+## NUTRI
 # nutri_food_endpoint = "https://trackapi.nutritionix.com/v2/natural/nutrients"
 nutri_exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"  # post request
 HEADERS = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
-    # "x-remote-user-id": "0",
 }
 
 # activity = input("What activity did you do: ")
 activity = "swimming 4km and running 2km"
 activity_params = {
     "query": activity,
-    "gender": "male",
-    "weight_kg": 70,
-    "height_cm": 180,
-    "age": 25,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE,
 }
 
-response = req.post(url=nutri_exercise_endpoint, json=activity_params, headers=HEADERS).json()
-print(response)
+nutri_response = req.post(url=nutri_exercise_endpoint, json=activity_params, headers=HEADERS).json()
+
+## SHEETY
+sheety_endpoint = "https://api.sheety.co"
+sheety_endpoint_complete = f"{sheety_endpoint}/{username}/{project_name}/{sheet_name}"
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for activity in nutri_response["exercises"]:
+    sheet_name_post = {
+        sheet_name[:-1]: {
+            "date": today_date,
+            "time": now_time,
+            "exercise": activity["name"],
+            "duration": activity["duration_min"],
+            "calories": activity["nf_calories"],
+        }
+    }
+
+    sheety_response = req.post(url=sheety_endpoint_complete, json=sheet_name_post)
+    print(f"POST: {sheety_response.json()['workout']}")
 
--- days_31-40/main.py
+++ days_31-40/main.py
@@ -1,18 +1,2 @@
-from datetime import datetime, timedelta
-
-def week_interval(date: tuple):
-    today = datetime(date[0], date[1], date[2])
-    start_week = datetime(today.year, today.month, today.day - (today.isoweekday() - 1))
-    end_week = start_week + timedelta(days=6)
-    return start_week.strftime("%Y%m%d"), end_week.strftime("%Y%m%d")
-
-
-date = (2022, 7, 24)
-print(week_interval(date))
-print(datetime(2022, 6, 30), type(datetime(2022, 6, 30)))
-
-
-# 18-24
-# 24 - (iso - 1) // 24 + ( 7 - iso)
-
-# 18 //
+for i in [1]:
+    print(i)
