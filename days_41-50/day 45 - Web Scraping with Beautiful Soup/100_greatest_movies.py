4123785f5db92a6b7653bdf3e8658195062340ea
quenrythane <artur.babinski.g@gmail.com>
2022-08-02 21:21:45 +0200

    finished quest

--- days_41-50/day 45 - Web Scraping with Beautiful Soup/100_greatest_movies.py
+++ days_41-50/day 45 - Web Scraping with Beautiful Soup/100_greatest_movies.py
@@ -1,10 +1,25 @@
 from bs4 import BeautifulSoup
 import requests as req
+import re
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
 response = req.get(URL)
 
 soup = BeautifulSoup(response.text, 'lxml')
-print(soup.prettify())
+print(soup)
 
+site_data = str(soup.find(id="__NEXT_DATA__").contents[0])
+start_indexes = [occurrence.start() + 13 for occurrence in re.finditer('"titleText":"', site_data)]
+
+for start_index in start_indexes[::-1][1:-1]:
+    end_index = site_data.find('"', start_index)
+    title = site_data[start_index:end_index]
+
+    try:
+        file = open('movies.txt', 'a')
+    except FileNotFoundError:
+        file = open('movies.txt', 'w')
+    finally:
+        file.write(f"{title}\n")
+        file.close()
