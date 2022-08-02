e78a5a2f8058e3ff6d6d0a9c70bd2e3ec5e196ae
quenrythane <artur.babinski.g@gmail.com>
2022-08-02 19:44:55 +0200

    current practice with bs- find/find all classes, etc

--- days_41-50/day 45 - Web Scraping with Beautiful Soup/main.py
+++ days_41-50/day 45 - Web Scraping with Beautiful Soup/main.py
@@ -6,6 +6,27 @@ with open('website.html', encoding="utf8") as html_file:
 
 soup = BeautifulSoup(contents, 'lxml')  # instead of lxml i could use html.parser
 
-print(soup.title)
-print(soup.title.name)
-print(soup.title.string)
+print(soup.title)  # <title>The HTML5 Herald</title>
+print(soup.title.name)  # title (tag name)
+print(soup.title.string)  # The HTML5 Herald
+print()
+
+# .find <- find fisrt atribute with name
+# .find_all <- find all atributes match the query
+all_links = soup.find_all('a')
+print(all_links)
+for link in all_links:
+    print(f"{link.string} - {link['href']}")
+    # print(f"{link.getText()} - {link.get('href')}")
+print()
+
+heading = soup.find(name='h1', id='name')
+print(heading, heading.string)
+print()
+
+find_class = soup.find_all(class_='heading')
+print(find_class)
+print(find_class[0].get('class'))
+print()
+
+
