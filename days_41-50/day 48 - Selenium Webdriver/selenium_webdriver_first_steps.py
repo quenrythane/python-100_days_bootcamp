a94531a7a2849fbc3eff91ceba88af72c17737d2
quenrythane <artur.babinski.g@gmail.com>
2022-08-04 21:25:17 +0200

    fisrt successfuly use

--- days_41-50/day 48 - Selenium Webdriver/selenium_webdriver_first_steps.py
+++ days_41-50/day 48 - Selenium Webdriver/selenium_webdriver_first_steps.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+# chrome driver download https://chromedriver.chromium.org/downloads
+
 # i love stackoverflow xd
 # https://stackoverflow.com/questions/47148872/webdrivers-executable-may-have-wrong-permissions-please-see-https-sites-goo
 # https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python
@@ -11,3 +13,7 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://www.amazon.de/")
 
+
+
+# drive.close()  # close tab
+# drive.quit()  # close browser
