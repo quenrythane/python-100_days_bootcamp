cc9e37333ececd60146bbdaa2bda1583495718b7
quenrythane <artur.babinski.g@gmail.com>
2022-08-04 21:18:47 +0200

    little help with stack overflow

---
+++ days_41-50/day 48 - Selenium Webdriver/selenium_webdriver_first_steps.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# i love stackoverflow xd
+# https://stackoverflow.com/questions/47148872/webdrivers-executable-may-have-wrong-permissions-please-see-https-sites-goo
+# https://stackoverflow.com/questions/64717302/deprecationwarning-executable-path-has-been-deprecated-selenium-python
+
+chrome_driver_path = 'C:\\Users\\Thane Art\\dev\\Tools\\chromedriver.exe'
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://www.amazon.de/")
+
