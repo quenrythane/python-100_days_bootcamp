a3bde809affdb2086045793ed6f9784189ec4de2
quenrythane <artur.babinski.g@gmail.com>
2022-08-04 19:57:00 +0200

    skip day 46

--- days_41-50/day 46 - Time Machine/time_machine.py
+++ days_41-50/day 46 - Time Machine/time_machine.py
@@ -1,1 +1,14 @@
-print('hello')
+from bs4 import BeautifulSoup
+import lxml
+import requests as req
+
+
+# period = input("what year you would like to travel to (YYY-MM-DD format): ")
+# URL = f"https://www.billboard.com/charts/hot-100/{period}/"
+URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
+
+response = req.get(URL)
+soup = BeautifulSoup(response.text, 'lxml')
+print(soup.prettify())
+
+
