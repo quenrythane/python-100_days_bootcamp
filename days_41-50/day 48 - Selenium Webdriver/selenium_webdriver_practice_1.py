06de38b18505530e2300d1871c3026b884e73168
quenrythane <artur.babinski.g@gmail.com>
2022-08-05 08:29:31 +0200

    more selenium practice

---
+++ days_41-50/day 48 - Selenium Webdriver/selenium_webdriver_practice_1.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By # Very important to import this
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
