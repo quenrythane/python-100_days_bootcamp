b9abbde012d4fa8d5113a55214db4034aff0bad0
quenrythane <artur.babinski.g@gmail.com>
2022-08-07 21:12:25 +0200

    working on products table - refactoring

--- days_41-50/day 48 - Selenium Webdriver/automated_bot_cookie_clikcer/automated_bot_cookie_clicker.py
+++ days_41-50/day 48 - Selenium Webdriver/automated_bot_cookie_clikcer/automated_bot_cookie_clicker.py
@@ -6,11 +6,7 @@ from selenium.webdriver.common.by import By
 
 cookie_bot.clicking_cookie(20)
 
-products_table = driver.driver.find_element(by=By.CSS_SELECTOR, value="div#products.storeSection")
-products_table_rows = products_table.find_elements(by=By.CLASS_NAME, value="unlocked")  # this works
 # .product  .unlocked/.locked - see name/don`t see name .enabled/.disabled - can afford to buy/can't afford to buy
-products_table_rows = products_table.find_elements(by=By.CLASS_NAME, value="product.unlocked")  # this also works
-products_table_rows = products_table.find_elements(by=By.CSS_SELECTOR, value="div.product.unlocked")  # this also works
 products_table_rows = driver.driver.find_elements(by=By.CSS_SELECTOR, value="div#products.storeSection div.product.unlocked")
 
 print("-"*20)
