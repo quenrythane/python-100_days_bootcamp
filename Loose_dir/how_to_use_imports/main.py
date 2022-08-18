dce5277f59f5696b82091fa324cffba29a97f579
quenrythane <artur.babinski.g@gmail.com>
2022-08-18 22:29:16 +0200

    learn about import module

---
+++ Loose_dir/how_to_use_imports/main.py
@@ -0,0 +1,7 @@
+print(1)
+import my_module
+print(2)
+
+if __name__ == '__main__':
+    print(3)
+    print(4)
---
+++ Loose_dir/how_to_use_imports/my_module.py
@@ -0,0 +1,4 @@
+print(__name__, 1)
+
+if __name__ == '__main__':
+    print(__name__, 2)
