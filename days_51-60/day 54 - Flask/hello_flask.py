6c7681dfee4f8347998d5f9e0836938c5a4219ba
quenrythane <artur.babinski.g@gmail.com>
2022-08-20 01:53:06 +0200

    first draft of final project day 55

--- days_51-60/day 54 - Flask/hello_flask.py
+++ days_51-60/day 54 - Flask/hello_flask.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from my_flask_decorators import *
+from random import randint
 app = Flask(__name__)
 print(__name__)
 
@@ -9,13 +11,44 @@ def hello_world():
 
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
 def bye_world():
     return 'Bye Succeeded!'
 
 
-@app.route('/hello/<name>')
+@app.route('/hello/<name>')  # <name> means a variable
 def hello_name(name):
     return f'Hello {name.capitalize()}!'
+
+
+"""
+@app.route('/hello/<path:name>')  # read variable as a path
+@app.route('/hello/<int:name>')  # read variable as a integer
+def hello_name(name):
+    return f'Hello {name.capitalize()}!'
+"""
+
+
+# decorator exercise
+@app.route("/woman/<woman>")
+@is_woman
+def woman(woman):
+    return woman
+
+
+# final guess a number project
+@app.route("/higher-lower")
+def higher_lower_root():
+    return "Guess a number between 0 and 9"
+
+
+@app.route("/higher-lower/<number>")
+def higher_lower_guess():
+    return "Guess a number between 0 and 9"
+
+
 
 
 if __name__ == '__main__':
---
+++ days_51-60/day 54 - Flask/my_flask_decorators.py
@@ -0,0 +1,37 @@
+def make_bold(function):
+    def wrapped():
+        return f"<b>{function()}</b>"
+    return wrapped
+
+
+def make_emphasis(function):
+    def wrapped():
+        return f"<em>{function()}</em>"
+    return wrapped
+
+
+def make_underlined(function):
+    def wrapped():
+        return f"<u>{function()}</u>"
+    return wrapped
+
+
+def is_woman(function):
+    def wrapped2(*args):
+        if args[0][-1] == "a":
+            name = f"Mrs {args[0].capitalize()}"
+            function(name)
+        else:
+            name = f"Mr {args[0].capitalize()}"
+            function(name)
+    return wrapped2
+
+@is_woman
+def tak(name):
+    print("Hi", name)
+
+tak("arta")
+tak("art")
+
+
+
