8edd1a88cf975ed2b97a4190be3ff3f610657fca
quenrythane <artur.babinski.g@gmail.com>
2022-08-19 07:56:15 +0200

    adding url variables and debug mode turned on

--- days_51-60/day 54 - Flask/hello_flask.py
+++ days_51-60/day 54 - Flask/hello_flask.py
@@ -2,14 +2,22 @@ from flask import Flask
 app = Flask(__name__)
 print(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello Succeeded!'
+
 
 @app.route('/bye')
 def bye_world():
     return 'Bye Succeeded!'
 
-if __name__ == '__main__':
-    app.run()
 
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f'Hello {name.capitalize()}!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # app.run(host="" , port=5000)
