e6759f3f70a49eb21a78cd83f6f2b8f2b01f882f
quenrythane <artur.babinski.g@gmail.com>
2022-08-20 23:18:01 +0200

    finished final project for day 57

--- days_51-60/main.py
+++ days_51-60/main.py
@@ -1,1 +1,13 @@
-text = "sposobów na przyciągnięcie najlepszych pracowników do Twojej firmy"
+import requests as req
+
+
+url = "https://api.npoint.io/c790b4d5cab58020d391"
+blog_response = req.get(url).json()
+
+index = 2
+
+post_data = blog_response[index-1]
+
+print(post_data)
+print(post_data["title"])
+# print(post_data.body)
