e4904dee5f99dd7b856140b3667549e6a36f0f75
quenrythane <artur.babinski.g@gmail.com>
2022-08-20 02:08:15 +0200

    finished final project day 55

--- days_51-60/day 54 - Flask/hello_flask.py
+++ days_51-60/day 54 - Flask/hello_flask.py
@@ -39,16 +39,26 @@ def woman(woman):
 
 
 # final guess a number project
+number = randint(0, 9)
+
+
 @app.route("/higher-lower")
 def higher_lower_root():
-    return "Guess a number between 0 and 9"
+    return "Guess a number between 0 and 9" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
 
 
-@app.route("/higher-lower/<number>")
-def higher_lower_guess():
-    return "Guess a number between 0 and 9"
-
-
+@app.route("/higher-lower/<int:guess_number>")
+def higher_lower_guess(guess_number):
+    if guess_number > number:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>"
+    elif guess_number < number:
+        return "<h1 style='color: red'>Too low, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>"
 
 
 if __name__ == '__main__':
