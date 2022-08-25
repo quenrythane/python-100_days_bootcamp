a3ace48fdf2d60988eb3c399b82589b6e4bf0f5e
quenrythane <artur.babinski.g@gmail.com>
2022-08-25 20:19:37 +0200

    figuring out flask

---
+++ days_51-60/day 59 recall - Templating Flask with Jinja/recall/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import requests as req
+
+app = Flask(__name__)
+url = "https://api.npoint.io/c790b4d5cab58020d391"
+blog_response = req.get(url).json()
+
+
+@app.route('/blog')
+@app.route('/')
+def home():
+    return render_template("index.html",
+                           posts=blog_response)
+
+@app.route('/post/<int:index>')
+def post(index):
+    post_data = blog_response[index-1]
+    return render_template("post.html",
+                           title=post_data["title"],
+                           body=post_data["body"],
+                           subtitle=post_data["subtitle"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=100)
