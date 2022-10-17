efaa56f358d47decfb5eeb02a09f5fbbc4ba74be
quenrythane <artur.babinski.g@gmail.com>
2022-10-17 21:43:58 +0200

    first draft of login form

---
+++ days_61-70/day 61 - Flask extension - Flask-WTF/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from wtforms import Form, BooleanField, StringField, validators
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+# wtforms
+class RegistrationForm(Form):
+    email = StringField('Email:', [validators.Length(min=6, max=35)])
+    password = StringField('Password:', [validators.Length(min=4, max=25)])
+
+
+@app.route("/login")
+def login():
+    login_form = RegistrationForm()
+    return render_template('login.html', form=login_form)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=61)
