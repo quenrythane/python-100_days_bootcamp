8b93ad59e5528d4c04c4cc4c1cd9635c6854dd27
quenrythane <artur.babinski.g@gmail.com>
2022-10-19 23:17:41 +0200

    flask-bootstrap

--- days_61-70/day 61 - Flask extension - Flask-WTF/main.py
+++ days_61-70/day 61 - Flask extension - Flask-WTF/main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
-from wtforms import Form, PasswordField, StringField, validators
+from flask import Flask, render_template, request
+from wtforms import Form, PasswordField, StringField, validators, SubmitField
+# from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 
@@ -10,16 +13,31 @@ def home():
 
 
 # wtforms
-class RegistrationForm(Form):
-    email = StringField('Email:', [validators.Length(min=6, max=35)])
-    password = PasswordField('Password:', [validators.Length(min=4, max=25)])
+class LoginForm(Form):
+    email = StringField('Email:', [validators.Email()])  # , default="artur.babinski.g@gmail.com")  # is this message doesn't work?
+    password = PasswordField(label='Password:', validators=[validators.Length(min=8, message=("wrong length"))])  # , default="zaq1@WSX"
+    submit = SubmitField("Log In")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    login_form = RegistrationForm()
+    login_form = LoginForm()
+    if request.method == 'POST':  # and login_form.validate():
+        print("1 email data:", login_form.email.data)
+        print("2 password data:", login_form.password.data)
+        print("3 validate:", login_form.validate())
+        if 1:
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
+    login_form.validate()
     return render_template('login.html', form=login_form)
 
+
+@app.route("/denied", methods=["GET", "POST"])
+def denied():
+    return render_template("denied.html")
 
 
 
